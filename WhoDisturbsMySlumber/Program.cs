722bd5e9653e3280b70a483597bfb13e1b21699c
Nicolas Ménard-Guy <nmenardg@gmail.com>
2016-09-23 19:18:24 -0400

    Initial commit

---
+++ WhoDisturbsMySlumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoDisturbsMySlumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runAs = Environment.UserName;
+            var identity = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var parentProcessPath = ParentProcess.FullPath;
+            var parentProcessName = ParentProcess.ProcessName;
+
+            File.AppendAllText(DateTime.Today.ToString("yyyy-MM-dd") + ".log", String.Format(@"{0}
+Run as:     {1}
+Identity:   {2}
+Parent process:
+- Path:     {3}
+- Name:     {4}
+
+", DateTime.Now, runAs, identity, parentProcessPath, parentProcessName));
+        }
+    }
+}
